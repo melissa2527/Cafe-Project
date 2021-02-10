bd2fadd53ce558e3018404b27c8f2cb2e1f0ba08
annie2527 <you@example.com>
2021-02-10 17:48:14 +0100

    scroll

--- src/components/home-page/motto/Motto.jsx
+++ src/components/home-page/motto/Motto.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './motto.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export const Motto = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 1500})
+    }, [])
+
     return (
         <div className='container'>
             <div className="row">
-            <div className="col motto four-square">
+            <div className="col motto four-square" data-aos='fade-up'>
                 <h2>Our Motto</h2>
                 <p>You can't buy happiness</p>
                 <p>But you CAN buy coffee</p>
                 <p>And that's pretty close</p>
             </div>
-            <div className="col four-square img-div">
+            <div className="col four-square img-div" data-aos='fade-right'>
                 <img src='./img/coffeetrio.jpg' alt='Huntington Beach'/>
             </div>
         </div>
         <div className="row">
-            <div className="col four-square img-div">
+            <div className="col four-square img-div" data-aos='fade-left'>
                 <img src='./img/huntingtonbeach.jpg' alt='Huntington Beach' />
             </div>
-            <div className="col motto four-square">
+            <div className="col motto four-square" data-aos='fade-down'>
                 <h2>Enjoy</h2>
                 <p>Stop in for a nice cup of coffee with a view of the famous Huntington Beach pier.  Since 2010, we have been serving fresh brews and cool gear.</p>
             </div>
